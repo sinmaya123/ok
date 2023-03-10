c2617f4b269f311ebec48e70547546e8f3fb4724
sinmaya123 <95042906+sinmaya123@users.noreply.github.com>
2023-03-10 17:44:50 +0530

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,chnnels = frame.shape
+size = (width,height)
+
+out = cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'DIVX'),5,size)
+sunrise = cv2.VideoWriter('sunrise.mp4',cv2.VideoWriter_fourcc(*'DIVX'),5,size)
+
+for i in range(0,count-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+for i in range(count-1,0,-1):
+    frame = cv2.imread(images[i])
+    sunrise.write(frame)
+
+out.release()
+sunrise.release()
+print("done")
